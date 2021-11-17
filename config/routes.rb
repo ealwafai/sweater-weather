04b4b78d634e82eaa849ca662c86ebd20198e533
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-11-16 21:14:45 -0600

    Update to RESTful

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,7 @@ Rails.application.routes.draw do
       resources :users, only: :create
       resources :sessions, only: :create
       resources :road_trip, only: :create, controller: :roadtrips
-      
-      get '/activities', to: 'activities#index'
+      resources :activities, only: :index
     end
   end
 end
